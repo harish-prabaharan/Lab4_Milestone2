35d5ec0e9ecc98e32d3356111213ce0dfd9c0148
harish-prabaharan <prabaharanharish@gmail.com>
2021-10-11 21:45:54 -0500

    accidentally wrote LocationManager instead of LocationListener on line 28,

--- app/src/main/java/com/example/lab4_milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab4_milestone2/MainActivity.java
@@ -25,7 +25,7 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
-    LocationManager locationListener;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
